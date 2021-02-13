f06f81d02416ba0fe2f85af1e01837c353af1122
Yuki-0505 <2243104010@qq.com>
2021-02-13 18:23:09 +0800

    修改函数名

---
+++ parseUrl.js
@@ -0,0 +1,10 @@
+/**
+ * readUrl(filePath: string, callback: (err: Error, data: JSON) => void): void
+ */
+function parseUrlFromHtml(filePath, callback) {
+
+}
+
+module.exports = {
+  parseUrlFromHtml
+}
